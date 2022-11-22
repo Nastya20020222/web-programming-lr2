e150ccefd83aa28b2edfde223ee9d9ccabec5edf
Aizhan Musaieva <musaieva.ru@gmail.com>
2022-11-22 10:11:07 +0700

    alert and party

--- lab6.js
+++ lab6.js
@@ -37,3 +37,36 @@ function setEnglish(){
     document.getElementById('ok-button').value='Ok';
     document.getElementById('reset-button').value='Reset';
 }
+
+function showAlert(){
+    let userName=document.getElementById('input-name').value;
+    let userSurname=document.getElementById('input-surname').value;
+    let userAge=document.getElementById('input-age').value;
+    alert('О, а я тебя знаю ты ' + userName+' '+userSurname+ '\n'+ 'Тебе '+userAge+' годиков');
+    let r =Math.round(Math.random()*255);
+    let g =Math.round(Math.random()*255);
+    let b =Math.round(Math.random()*255);
+    let color ='rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor=color;
+}
+function party1(){
+    let r =Math.round(Math.random()*255);
+    let g =Math.round(Math.random()*255);
+    let b =Math.round(Math.random()*255);
+    let color ='rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('header').style.backgroundColor=color;
+}
+function party2(){
+    let r =Math.round(Math.random()*255);
+    let g =Math.round(Math.random()*255);
+    let b =Math.round(Math.random()*255);
+    let color ='rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('student').style.backgroundColor=color;
+}
+function party3(){
+    let r =Math.round(Math.random()*255);
+    let g =Math.round(Math.random()*255);
+    let b =Math.round(Math.random()*255);
+    let color ='rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('footer').style.backgroundColor=color;
+}
