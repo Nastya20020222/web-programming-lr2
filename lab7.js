87a3b59076e79e555dfa0b727805b6072ab78edb
Студент Факультета Бизнеса <student@fb.local>
2022-12-06 10:40:01 +0700

    Снежинки и таблица умножения

--- lab7.js
+++ lab7.js
@@ -29,3 +29,53 @@ function showAlert(){
     let color ='rgb(' + r + ',' + g + ',' + b + ')';
     document.getElementById('user-data').style.backgroundColor=color;
 }
+
+function makeStairs(){
+    let stairs=document.querySelector('#stairs');
+    let stepCount=20;
+    for (let i=0; i<stepCount; i++){
+        stairs.innerHTML +='<div class="step"></div>'
+    }
+    let steps=document.querySelectorAll('.step');
+    for(let i=0; i<steps.length; i++){
+        steps[i].style.left=(400+10*i)+'px';
+        steps[i].style.top=(80+80*i)+'px';
+        steps[i].innerHTML='Блок номер '+(i+1);
+    }
+}
+
+function Snow(){
+    let snezki = document.querySelector(' #snezki');
+    let stepCount = 40;
+    for (let i = 0; i<stepCount; i++){
+    snezki.innerHTML += '<img class="snow" src="sneg84-KOPIY2dbbc-d258b739.png">';
+    }
+    let snezhinkis = document.querySelectorAll('.snow');
+    let screenWidth = window.screen.width;
+    let screenHeight = window.screen.height;
+
+    for (let i = 0; i<snezhinkis.length; i++)
+    {
+    snezhinkis[i].style.left = Math.random() * (screenWidth-100) + 'px';
+    snezhinkis[i].style.top = Math.random() *(screenHeight-100) + 'px';
+    snezhinkis[i].style.width= Math.random() * 45 + 'px';
+    }
+}
+
+function tableT(){
+    let stairs=document.querySelector('#table');
+    let stepCount=10;
+    for (let i=1; i<stepCount; i++){
+        stairs.innerHTML +='<div class="table"></div>'
+    }
+    let steps=document.querySelectorAll('.table');
+    for(let i=1; i<steps.length; i++){
+        steps[i].innerHTML=i+'*28='+(i*28);
+    }
+}
+
+function load (){
+    makeStairs();
+    Snow();
+    tableT();
+}
