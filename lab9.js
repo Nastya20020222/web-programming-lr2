c85016ecf42f52bf2c6335478da50fb37a1cdac8
Aizhan Musaieva <musaieva.ru@gmail.com>
2022-12-20 09:47:31 +0700

    Переключения состояния

--- lab9.js
+++ lab9.js
@@ -4,3 +4,16 @@ function clickMe(button){
     else
     button.innerHTML='ТЫК'
 }
+let name=document.getElementsByClassName('Ivan').value;
+function changeI(button){
+    if(button.innerHTML=='Мальков')
+    button.innerHTML='Иван';
+    else
+    button.innerHTML='Мальков'
+}
+function changeA(button){
+    if(button.innerHTML=='Макурова')
+    button.innerHTML='Анастасия';
+    else
+    button.innerHTML='Макурова'
+}
